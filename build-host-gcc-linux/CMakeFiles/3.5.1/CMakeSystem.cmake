72320f7bb1c3aec86edd36515a019ba6bf579bd6
wangpengcheng <wangpengcheng2018@gmail.com>
2019-07-12 20:48:15 +0800

    add build and dpkg

---
+++ build-host-gcc-linux/CMakeFiles/3.5.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-4.15.0-50-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-50-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/wangpengcheng/ncnn/toolchains/host.gcc.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
